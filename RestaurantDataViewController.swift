84905ec04b612a366c7aee0669dd8f1e1c713e80
lsecrease <lsecrease@gmail.com>
2015-10-04 17:01:32 -0500

    Added ImagePickerSheet VC functions

--- RestaurantDataViewController.swift
+++ RestaurantDataViewController.swift
@@ -8,22 +8,22 @@
 
 import Foundation
 import UIKit
-
+import Photos
 
 class RestaurantDataViewController: UIViewController, UIAlertViewDelegate,
     UINavigationControllerDelegate,
     UIImagePickerControllerDelegate {
 
+   
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     @IBOutlet weak var restImage1: UIImageView!
     @IBOutlet weak var restImage2: UIImageView!
     @IBOutlet weak var restImage3: UIImageView!
     @IBOutlet weak var restImage4: UIImageView!
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    @IBOutlet var buttonimage: [UIButton]!
-    
-    var buttTAG = Int()
+    private var postImage: UIImage! //Used to Store restImage image
     
     @IBOutlet weak var numberOfDishesSwiped: UILabel!
     @IBOutlet weak var checkInTotal: UILabel!
@@ -62,65 +62,222 @@ class RestaurantDataViewController: UIViewController, UIAlertViewDelegate,
         }
     }
     
-    
-    // BUTTON FOR IMAGES
-    func buttImageTapped(sender: UIButton) {
-        var button = sender as UIButton
-        buttTAG = button.tag
-        println("\(buttTAG)")
-        println("\(button.tag)")
-       
-        var alert = UIAlertView(title: "Dindr",
-            message: "Add a Photo",
-            delegate: self,
-            cancelButtonTitle: "Cancel",
-            otherButtonTitles:
-            "Take a picture",
-            "Choose from Library"
-        )
-        alert.show()
+    //Using ImagePickerSheet Controller
+    @IBAction func pickFeaturedImageClicked(sender: UITapGestureRecognizer) {
         
-    }
-    
-    // AlertView delegate
-    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        let authorization = PHPhotoLibrary.authorizationStatus()
         
-        // OPEN DEVICE'S CAMERA
-        if alertView.buttonTitleAtIndex(buttonIndex) == "Take a picture" {
-            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-                var imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
-                imagePicker.allowsEditing = false
-                self.presentViewController(imagePicker, animated: true, completion: nil)
-            }
-            
-            
-            // PICK A PHOTO FROM LIBRARY
-        } else if alertView.buttonTitleAtIndex(buttonIndex) == "Choose from Library" {
-            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
-                var imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
-                imagePicker.allowsEditing = false
-                self.presentViewController(imagePicker, animated: true, completion: nil)
-            }
+        if authorization == .NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                //self.pickFeaturedImageClicked(sender)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.pickFeaturedImageClicked(sender)
+                })
+            })
+            return
         }
+        //Do you want to Take a Photo or Video with the Camera
+        if authorization == .Authorized {
+            let controller = ImagePickerSheetController()
+            controller.addAction(ImageAction(title: NSLocalizedString("Take Photo or Video", comment: "ActionTitle"), secondaryTitle: NSLocalizedString("Use This One", comment: "ActionTitle"), handler: { (_) -> () in
+                
+                self.presentCamera()
+                
+                }, secondaryHandler: { (action, numberOfPhotos) -> () in
+                    controller.getSelectedImagesWithCompletion({ (images) -> Void in
+                        self.postImage = images[0]
+                        self.restImage1.image = self.postImage
+                    })
+            }))
+            
+            controller.addAction(ImageAction(title: NSLocalizedString("Cancel", comment: "Action Title"),  style: .Cancel))
+            
+            presentViewController(controller, animated: true, completion: nil)
+            
+        }
+        
     }
     
-    // ImagePicker Delegate
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+    func presentCamera() {
         
-        // Assign Images
-        switch buttTAG {
-        case 0: restImage1.image = image;   break
-        case 1: restImage2.image = image;   break
-        case 2: restImage3.image = image;   break
-        case 3: restImage4.image = image;   break
-        default: break
-        }
-        dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
+    //Using ImagePickerSheet Controller for 2nd Image
+    @IBAction func pick2ndFeaturedImageClicked(sender: UITapGestureRecognizer) {
+        
+        let authorization = PHPhotoLibrary.authorizationStatus()
+        
+        if authorization == .NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                //self.pickFeaturedImageClicked(sender)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.pickFeaturedImageClicked(sender)
+                })
+            })
+            return
+        }
+        //Do you want to Take a Photo or Video with the Camera
+        if authorization == .Authorized {
+            let controller = ImagePickerSheetController()
+            controller.addAction(ImageAction(title: NSLocalizedString("Take Photo or Video", comment: "ActionTitle"), secondaryTitle: NSLocalizedString("Use This One", comment: "ActionTitle"), handler: { (_) -> () in
+                
+                self.presentCamera()
+                
+                }, secondaryHandler: { (action, numberOfPhotos) -> () in
+                    controller.getSelectedImagesWithCompletion({ (images) -> Void in
+                        self.postImage = images[0]
+                        self.restImage2.image = self.postImage
+                    })
+            }))
+            
+            controller.addAction(ImageAction(title: NSLocalizedString("Cancel", comment: "Action Title"),  style: .Cancel))
+            
+            presentViewController(controller, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    //Using ImagePickerSheet Controller
+    @IBAction func pick3rdFeaturedImageClicked(sender: UITapGestureRecognizer) {
+        
+        let authorization = PHPhotoLibrary.authorizationStatus()
+        
+        if authorization == .NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                //self.pickFeaturedImageClicked(sender)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.pickFeaturedImageClicked(sender)
+                })
+            })
+            return
+        }
+        //Do you want to Take a Photo or Video with the Camera
+        if authorization == .Authorized {
+            let controller = ImagePickerSheetController()
+            controller.addAction(ImageAction(title: NSLocalizedString("Take Photo or Video", comment: "ActionTitle"), secondaryTitle: NSLocalizedString("Use This One", comment: "ActionTitle"), handler: { (_) -> () in
+                
+                self.presentCamera()
+                
+                }, secondaryHandler: { (action, numberOfPhotos) -> () in
+                    controller.getSelectedImagesWithCompletion({ (images) -> Void in
+                        self.postImage = images[0]
+                        self.restImage3.image = self.postImage
+                    })
+            }))
+            
+            controller.addAction(ImageAction(title: NSLocalizedString("Cancel", comment: "Action Title"),  style: .Cancel))
+            
+            presentViewController(controller, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    
+    //Using ImagePickerSheet Controller
+    @IBAction func pick4thFeaturedImageClicked(sender: UITapGestureRecognizer) {
+        
+        let authorization = PHPhotoLibrary.authorizationStatus()
+        
+        if authorization == .NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                //self.pickFeaturedImageClicked(sender)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.pickFeaturedImageClicked(sender)
+                })
+            })
+            return
+        }
+        //Do you want to Take a Photo or Video with the Camera
+        if authorization == .Authorized {
+            let controller = ImagePickerSheetController()
+            controller.addAction(ImageAction(title: NSLocalizedString("Take Photo or Video", comment: "ActionTitle"), secondaryTitle: NSLocalizedString("Use This One", comment: "ActionTitle"), handler: { (_) -> () in
+                
+                self.presentCamera()
+                
+                }, secondaryHandler: { (action, numberOfPhotos) -> () in
+                    controller.getSelectedImagesWithCompletion({ (images) -> Void in
+                        self.postImage = images[0]
+                        self.restImage4.image = self.postImage
+                    })
+            }))
+            
+            controller.addAction(ImageAction(title: NSLocalizedString("Cancel", comment: "Action Title"),  style: .Cancel))
+            
+            presentViewController(controller, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    
+    
+   
+
+    
+    
+//    // BUTTON FOR IMAGES
+//    func buttImageTapped(sender: UIButton) {
+//        var button = sender as UIButton
+//        buttTAG = button.tag
+//        println("\(buttTAG)")
+//        println("\(button.tag)")
+//       
+//        var alert = UIAlertView(title: "Dindr",
+//            message: "Add a Photo",
+//            delegate: self,
+//            cancelButtonTitle: "Cancel",
+//            otherButtonTitles:
+//            "Take a picture",
+//            "Choose from Library"
+//        )
+//        alert.show()
+//        
+//    }
+    
+//    // AlertView delegate
+//    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+//        
+//        // OPEN DEVICE'S CAMERA
+//        if alertView.buttonTitleAtIndex(buttonIndex) == "Take a picture" {
+//            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+//                var imagePicker = UIImagePickerController()
+//                imagePicker.delegate = self
+//                imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+//                imagePicker.allowsEditing = false
+//                self.presentViewController(imagePicker, animated: true, completion: nil)
+//            }
+//            
+//            
+//            // PICK A PHOTO FROM LIBRARY
+//        } else if alertView.buttonTitleAtIndex(buttonIndex) == "Choose from Library" {
+//            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+//                var imagePicker = UIImagePickerController()
+//                imagePicker.delegate = self
+//                imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+//                imagePicker.allowsEditing = false
+//                self.presentViewController(imagePicker, animated: true, completion: nil)
+//            }
+//        }
+//    }
+    
+    
+    
+//    // ImagePicker Delegate
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+//        
+//        // Assign Images
+//        switch buttTAG {
+//        case 0: restImage1.image = image;   break
+//        case 1: restImage2.image = image;   break
+//        case 2: restImage3.image = image;   break
+//        case 3: restImage4.image = image;   break
+//        default: break
+//        }
+//        dismissViewControllerAnimated(true, completion: nil)
+//    }
 
 
     @IBAction func editSpecialButtonTapped(sender: AnyObject) {
