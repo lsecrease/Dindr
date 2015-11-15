cdd1cc5971517d97da221998a691a1d50f8d2ce7
lsecrease <lawrenceolivier@Lawrences-MBP.attlocal.net>
2015-11-14 19:58:48 -0600

    EditSpecials

--- Dindr/EditSpecialsViewController.swift
+++ Dindr/EditSpecialsViewController.swift
@@ -12,18 +12,58 @@ class EditSpecialsViewController: UIViewController {
 
     @IBOutlet weak var perkDescriptionTextView: UITextView!
     
+    @IBOutlet var hideKeyboardInputAccessoryView: UIView!
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         perkDescriptionTextView.inputAccessoryView = hideKeyboardInputAccessoryView
 
-        // Do any additional setup after loading the view.
+        perkDescriptionTextView.becomeFirstResponder()
+        //perkDescriptionTextView.delegate = self
+       
+        
+        // handle text view
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    @IBAction func hideKeyboard()
+    {
+        if perkDescriptionTextView.isFirstResponder() {
+            perkDescriptionTextView.resignFirstResponder()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    // MARK: - Text View Handler
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification)
+    {
+        let userInfo = notification.userInfo ?? [:]
+        let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue().size
+        
+        self.perkDescriptionTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        self.perkDescriptionTextView.scrollIndicatorInsets = self.perkDescriptionTextView.contentInset
+    }
+    
+    func keyboardWillHide(notification: NSNotification)
+    {
+        self.perkDescriptionTextView.contentInset = UIEdgeInsetsZero
+        self.perkDescriptionTextView.scrollIndicatorInsets = UIEdgeInsetsZero
     }
     
 
 
 }
+
+
+
